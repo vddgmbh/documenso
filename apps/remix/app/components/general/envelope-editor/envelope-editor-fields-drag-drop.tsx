a1a3e8c8df1ec778b25b8a099aa9202096763f15
Vincent van den Eijkel <van.den.eijkel@gmail.com>
2026-03-15 00:37:32 +0100

    fix: guard

--- apps/remix/app/components/general/envelope-editor/envelope-editor-fields-drag-drop.tsx
+++ apps/remix/app/components/general/envelope-editor/envelope-editor-fields-drag-drop.tsx
@@ -293,7 +293,7 @@ export const EnvelopeEditorFieldDragDrop = ({
             <p
               className={cn(
                 'flex items-center justify-center gap-x-1.5 font-noto text-sm font-normal text-muted-foreground group-data-[selected]:text-foreground',
-                field.className,
+                'className' in field && field.className,
                 selectedRecipientStyles.fieldButtonText,
               )}
             >
